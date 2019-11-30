25e626098f404cc8c19f445fe1e6d1a17f8f29b2
unknown <utkarshr2811@gmail.com>
2019-11-30 22:34:29 +0530

    Added slight functionality

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -12,6 +14,10 @@ const io = socketio(server);
 
 io.on('connection', (socket) =>{
     console.log('We have a new connection!!!!');
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+    });
 
     socket.on('disconnect', () => {
         console.log('User has left!!!!');
